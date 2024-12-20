f5c2bc79b3583ece17c3354de78283a4c57d8df2
hegedusmarcell15 <hegedusm@kkszki.hu>
2024-12-20 10:59:08 +0100

    The programe is ready to use

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Google.Protobuf;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using UserKezeles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace User_kezelés
 {
@@ -16,5 +21,63 @@ namespace User_kezelés
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string connString = "Server=localhost;Database=user;Uid=root;Pwd=;";
+
+  
+            string query = "SELECT * FROM `data`";
+
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                    listBox1.Items.Clear();
+
+
+                    while (reader.Read())
+                    {
+
+                        string fname = reader["FirstName"].ToString();
+                        string lname = reader["LastName"].ToString();
+                        int password = int.Parse(reader["Password"].ToString());
+                        string created = reader["CreatedTime"].ToString();
+                        string updated = reader["UpdatedTime"].ToString();
+
+
+
+                        string displayText = $"Vezetéknév: {fname}, Keresztnév: {lname}, Jelszava: {password}, Adatbázisba felvéve: {created}, Módosítva: {updated}";
+
+
+                        listBox1.Items.Add(displayText);
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hiba történt: " + ex.Message);
+                }
+                conn.Close();
+            }
+        }
     }
 }
+   
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserKezeles;
 
 namespace User_kezelés
 {
@@ -13,11 +14,12 @@ namespace User_kezelés
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+            static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
     }
 }
